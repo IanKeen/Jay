78c97060c1f8c2d457028abdb2b0c7341bbda359
Ian Keen <iankeen82@gmail.com>
2016-06-08 17:09:56 -0700

    downgraded dependency; imported C7.Json from 0.8

--- Package.swift
+++ Package.swift
@@ -7,6 +7,6 @@ let package = Package(
         Target(name: "JayExample", dependencies: [.Target(name: "Jay")])
     ],
     dependencies: [
-    	.Package(url: "https://github.com/open-swift/C7.git", versions: Version(0,8,0)..<Version(1,0,0))
+    	.Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 7)
     ]
 )
